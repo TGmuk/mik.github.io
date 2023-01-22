1f0597717d3a5c1e41871baebbd97906f34b7bc9
justtn1 <mail@justn.eu>
2023-01-22 01:03:17 +0100

    added rotation

---
+++ js/code.js
@@ -0,0 +1,27 @@
+
+alert("Hey");
+
+var input = document.getElementById("angle");
+var btn = document.getElementById("btnGo");
+var needle = document.getElementById("mikbilddiv");
+
+
+
+// Set up click event handler
+btn.addEventListener("click", start_animation);
+
+// Storage for the previous angle
+var lastAngle = "";
+
+
+function start_animation(){ 
+// Update the total angle needed
+lastAngle = +lastAngle + +input.value;
+
+// For testing:
+console.clear()
+console.log("Current total angle: " + lastAngle);
+
+// Move the needle:
+needle.style.transform = "rotate(" + lastAngle + "deg)";
+}
