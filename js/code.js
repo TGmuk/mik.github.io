ffd44d990192efd535518a98c39d0b98c16d49f4
DJSkyRoad <jakobpauls2@gmail.com>
2023-01-22 13:46:46 +0100

    remove dark mode toggle

--- js/code.js
+++ js/code.js
@@ -1,11 +1,8 @@
-
 alert("Hey");
 
 var input = document.getElementById("angle");
 var btn = document.getElementById("btnGo");
 var needle = document.getElementById("mikbilddiv");
-
-
 
 // Set up click event handler
 btn.addEventListener("click", start_animation);
@@ -25,29 +22,3 @@ function start_animation() {
     // Move the needle:
     needle.style.transform = "rotate(" + lastAngle + "deg)";
 }
-
-let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-const enableDarkMode = () => {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("darkMode", "enabled");
-}
-
-const disableDarkMode = () => {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("darkMode", null);
-}
-
-if (darkMode === "enabled") {
-    enableDarkMode();
-}
-
-darkModeToggle.addEventListener("click", () => {
-    darkMode = localStorage.getItem("darkMode");
-    if (darkMode !== null) {
-        disableDarkMode();
-    } else {
-        enableDarkMode();
-    }
-});
